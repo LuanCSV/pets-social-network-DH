95a0719a89d2f6330c4f798156b58c98c64ab6fc
Luan Vasconcelos <luancsvasconcelos@hotmail.com>
2020-11-23 08:04:00 -0300

    pequenas mudancas

--- frontend-react/src/components/WelcomePage/WelcomePage.js
+++ frontend-react/src/components/WelcomePage/WelcomePage.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import './WelcomePage.css';
-import Icon from '../../assets/icons/icon.png';
+import Logo from '../Logo/Logo';
 
 const WelcomeComponent = () => {
   return (
     <div className='bodyWelcome'>
       <main className="mainSection">
         <div className="header">
-          <h1>PetBook</h1> <img src={Icon} alt="Logo do Petbook" />
+          <Logo size="45"/>
           <p>Uma rede social para <br /> o seu pet.</p>
         </div>
         <div class="action">
