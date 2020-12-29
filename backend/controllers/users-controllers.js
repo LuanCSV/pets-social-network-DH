a050de4a5cae2b20bc88219bb6de7f7d22e8395d
Luan Vasconcelos <luancsvasconcelos@hotmail.com>
2020-12-29 17:05:29 -0300

    GET e POST com valores fixos criados

--- backend/controllers/users-controllers.js
+++ backend/controllers/users-controllers.js
@@ -1,8 +1,65 @@
 
+const User = require('../models/user');
+
+
 const UsersControllers = {
-    getUsers: (req, res, next) => {
-        
+    getUsers: async (req, res, next) => {
+
+        let users;
+        try {
+            users = await User.find({}, '-senha');
+        } catch (error) {
+            console.log(error);
+            return next(error);
+        }
+        res.json(
+            {
+                message: "Users",
+                // getter: adiciona o "id" como string para nos conseguirmos utiliza-lo
+                // ja que o _id eh apenas para o mongodb
+                users: users.map(user => user.toObject({ getters: true }))
+            }
+        );
     },
+    signup: async (req, res, next) => {
+        const { name, email, password } = req.body;
+
+        // verifica se ja existe um usuario no banco de dados
+        let existingUser;
+        try {
+            existingUser = await User.findOne({ email: email });
+        } catch (error) {
+            return next(error);
+        }
+
+        if (existingUser) {
+            return res.json({ message: "Usuario ja existe" });
+        }
+
+        // cria o usuario q vai ser salvo no banco de dados
+        const createdUser = new User({
+            admin: true,
+            name: name,
+            email: email,
+            password: password,
+            image: "https://upload.wikimedia.org/wikipedia/commons/7/70/User_icon_BLACK-01.png",
+            // createdAt:,
+            local: " ",
+            following: [],
+            followers: [],
+            pets: [],
+            posts: []
+        });
+
+        try {
+            await createdUser.save();
+        } catch (err) {
+            console.log(err);
+            return next();
+        }
+
+        res.json({ user: createdUser })
+    }
 }
 
 module.exports = UsersControllers;
--- backend/routes/users-routes.js
+++ backend/routes/users-routes.js
@@ -2,12 +2,10 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-    res.json(
-        {
-            message: "Users"
-        }
-    );
-});
+const userController = require('../controllers/users-controllers');
+
+router.get('/', userController.getUsers);
+
+router.post('/', userController.signup);
 
 module.exports = router;
