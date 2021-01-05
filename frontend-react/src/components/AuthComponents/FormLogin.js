d91a788cbf394459e2750c1d7d00688694743ac0
Luan Vasconcelos <luancsvasconcelos@hotmail.com>
2021-01-05 11:51:50 -0300

    Login sisimplificado no frontend e backend

--- frontend-react/src/components/AuthComponents/FormLogin.js
+++ frontend-react/src/components/AuthComponents/FormLogin.js
@@ -37,9 +37,33 @@ const FormLogin = () => {
         }
     }
 
-    const loginSubmitHandler = (event) => {
+    const loginSubmitHandler = async (event) => {
         event.preventDefault();
         console.table(formLoginState);
+        let valuesFormCopy = formLoginState;
+
+        let body = JSON.stringify({
+            email: valuesFormCopy.Email,
+            password: valuesFormCopy.Senha
+        })
+
+        try {
+            const response = await fetch(
+                `http://localhost:5000/users/login`,
+                {
+                    method: 'POST',
+                    body: body,
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                }
+            )
+
+            const responseData = await response.json()
+            alert(responseData.error || responseData.message)
+        } catch (error) {
+            console.log(error);
+        }
     }
 
     return (
