52ad63b2360d044b53013ff84e73dd7eec118b05
Luan Vasconcelos <luancsvasconcelos@hotmail.com>
2021-01-06 09:48:31 -0300

    navlinks criados no mainNavigation

--- frontend-react/src/components/MainNavigation/MainNavigation.js
+++ frontend-react/src/components/MainNavigation/MainNavigation.js
@@ -1,13 +1,24 @@
 
 import './MainNavigation.css'
 
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
 import Logo from '../../shared/Logo/Logo'
+
+const getWidthWindow = () => window.innerWidth;
 
 function MainNavigation() {
 
+    const [windowWidth, setWindowWidth] = useState(getWidthWindow);
+
+    useEffect(() => {
+        const handleWidth = () => setWindowWidth(getWidthWindow)
+        window.addEventListener('resize', handleWidth);
+        return () => window.removeEventListener('resize', handleWidth);
+    }, []);
+
     let menu;
-    if (true) {
+    if (windowWidth < 768) {
         menu = (
             <>
                 <Logo icon size="40" padding="0" />
@@ -16,22 +27,35 @@ function MainNavigation() {
                     Pagina atual
                 </div>
 
-                <div className="imageUser">
-                    <img src="https://digimedia.web.ua.pt/wp-content/uploads/2017/05/default-user-image.png" alt="" />
+                <div className="userMenuBox">
+                    <button className="buttonUser">
+                        <img src="https://digimedia.web.ua.pt/wp-content/uploads/2017/05/default-user-image.png" alt="" />
+                    </button>
                 </div>
             </>
         )
     } else {
         menu = (
             <>
-                <Logo size="40" padding="0" />
+                <Logo size="25" padding="0" />
 
-                <div className="currentPage">
-                    Pagina atual
+                <div className="navLinksGroup">
+                    <Link to="/profile/123" className="navLinks">
+                        Admin
+                    </Link>
+                    <Link to="/admin" className="navLinks">
+                        Feed
+                    </Link>
+                    <Link to="/admin" className="navLinks">
+                        Explorar
+                    </Link>
                 </div>
 
-                <div className="imageUser">
-                    <img src="https://digimedia.web.ua.pt/wp-content/uploads/2017/05/default-user-image.png" alt="" />
+                <div className="userMenuBox">
+                    <button className="buttonUser desktop">
+                        <p>Luan Carlos da Silva Vasconcelos</p>
+                        <img src="https://digimedia.web.ua.pt/wp-content/uploads/2017/05/default-user-image.png" alt="" />
+                    </button>
                 </div>
             </>
         )
