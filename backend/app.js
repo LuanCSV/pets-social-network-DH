ce6fe9992c6f2b6ead8ca8ec49bbd123e8c61f4f
Luan Vasconcelos <luancsvasconcelos@hotmail.com>
2021-01-14 16:03:19 -0300

    teste .env

--- backend/app.js
+++ backend/app.js
@@ -6,10 +6,6 @@ const app = express();
 // ### Configuracoes ###
 const port = process.env.PORT || 5000;
 const MONGO_URL = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@mean-course.ocusw.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
-// const DB_USERNAME = 'PetBookAdmin';
-// const DB_PASSWORD = 'vsk1Mfu6aO83ZlEj';
-// const DB_NAME = 'PetbookDB';
-// const MONGO_URL = `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@mean-course.ocusw.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`;
 
 const usersRoutes = require('./routes/users-routes');
 
