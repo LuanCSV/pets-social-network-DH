bfcbbb7b40304bedd31141c8c7238c0100c70e3c
Luan Vasconcelos <luancsvasconcelos@hotmail.com>
2021-01-14 15:59:29 -0300

    dotenv adicionado

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
+require("dotenv").config();
 const app = express();
 
 // ### Configuracoes ###
 const port = process.env.PORT || 5000;
-// const MONGO_URL = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@mean-course.ocusw.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
-const DB_USERNAME = 'PetBookAdmin';
-const DB_PASSWORD = 'vsk1Mfu6aO83ZlEj';
-const DB_NAME = 'PetbookDB';
-const MONGO_URL = `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@mean-course.ocusw.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`;
+const MONGO_URL = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@mean-course.ocusw.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+// const DB_USERNAME = 'PetBookAdmin';
+// const DB_PASSWORD = 'vsk1Mfu6aO83ZlEj';
+// const DB_NAME = 'PetbookDB';
+// const MONGO_URL = `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@mean-course.ocusw.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`;
 
 const usersRoutes = require('./routes/users-routes');
 
@@ -50,5 +50,6 @@ mongoose
             console.log('Servidor iniciado!');
         })
     ).catch(err => {
+        
         console.log(err);
     });
