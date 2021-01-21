ba544510753374e288d3e6ac47692b3c3e14878f
Guilherme Martins <guimartinsstyle@gmail.com>
2021-01-21 15:15:46 -0300

    Criado criptografia para a senha

--- backend/controllers/users-controllers.js
+++ backend/controllers/users-controllers.js
@@ -1,129 +1,127 @@
+const User = require("../models/user");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
-const User = require('../models/user');
-const jwt = require('jsonwebtoken');
-
-const super_secret = "petbooksupersecret"
-
+const super_secret = "petbooksupersecret";
 
 const UsersControllers = {
-    getUsers: async (req, res, next) => {
-
-        let users;
-        try {
-            users = await User.find({}, '-senha');
-        } catch (error) {
-            console.log(error);
-            return next(error);
-        }
-        res.json(
-            {
-                // getter: adiciona o "id" como string para nos conseguirmos utiliza-lo
-                // ja que o _id eh apenas para o mongodb
-                users: users.map(user => user.toObject({ getters: true }))
-            }
-        );
-    },
-    signup: async (req, res, next) => {
-        const { name, email, password } = req.body;
-
-        // verifica se ja existe um usuario no banco de dados
-        let existingUser;
-        try {
-            existingUser = await User.findOne({ email: email });
-        } catch (error) {
-            return next(error);
-        }
-
-        if (existingUser) {
-            return res.json({ error: "Usuario ja existe" });
-        }
-
-        // cria o usuario q vai ser salvo no banco de dados
-        const createdUser = new User({
-            admin: false,
-            name: name,
-            email: email,
-            password: password,
-            image: "https://upload.wikimedia.org/wikipedia/commons/7/70/User_icon_BLACK-01.png",
-            // createdAt:,
-            local: " ",
-            following: [],
-            followers: [],
-            pets: [],
-            posts: []
-        });
-
-        try {
-            await createdUser.save();
-        } catch (err) {
-            console.log(err);
-            return next();
-        }
-
-        res.json({ message: "Usuario criado com sucesso", user: createdUser })
-    },
-    login: async (req, res, next) => {
-        const { email, password } = req.body;
-
-        // procurando se usuario existe
-        let authenticatedUser;
-        try {
-            authenticatedUser = await User.findOne({ email: email });
-        } catch (err) {
-            return next(err);
-        }
-
-        if (!authenticatedUser) {
-            return res.json({ error: "Usuario nao existe" });
-        }
-
-        // antes de usar o hash na senha
-        if (authenticatedUser.password !== password) {
-            return res.json({ error: "Senha incorreta" });
-        }
-
-        // aqui vai vir a autenticacao
-        let token;
-        try {
-            token = jwt.sign(
-                { userId: authenticatedUser.id, email: authenticatedUser.email }
-                , super_secret,
-                { expiresIn: "1hr" }
-            )
-        } catch (err) {
-            console.log(err);
-            return res.json({ error: "Erro interno" });
-        }
-
-        // response
-        res.json({
-            message: "Logged in! - Acessou!",
-            userId: authenticatedUser.id,
-            email: authenticatedUser.email,
-            admin: authenticatedUser.admin,
-            token: token
-        });
-
-    },
-    getUser: async (req, res, next) => {
-
-        const userId = req.params.id;
-
-        let user;
-        try {
-            user = await User.findById(userId, '-password');
-        } catch (error) { }
-
-        if (!user) {
-            return res.json({
-                error: "Nao foi possivel achar o usuario"
-            })
-        }
-
-        res.json({
-            data: user.toObject({ getters: true })
-        })
+  getUsers: async (req, res, next) => {
+    let users;
+    try {
+      users = await User.find({}, "-senha");
+    } catch (error) {
+      console.log(error);
+      return next(error);
     }
-}
+    res.json({
+      // getter: adiciona o "id" como string para nos conseguirmos utiliza-lo
+      // ja que o _id eh apenas para o mongodb
+      users: users.map((user) => user.toObject({ getters: true })),
+    });
+  },
+  signup: async (req, res, next) => {
+    const { name, email, password } = req.body;
+
+    // verifica se ja existe um usuario no banco de dados
+    let existingUser;
+    try {
+      existingUser = await User.findOne({ email: email });
+    } catch (error) {
+      return next(error);
+    }
+
+    if (existingUser) {
+      return res.json({ error: "Usuario ja existe" });
+    }
+
+    // cria o usuario q vai ser salvo no banco de dados
+    const createdUser = new User({
+      admin: false,
+      name: name,
+      email: email,
+      password: password,
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/7/70/User_icon_BLACK-01.png",
+      // createdAt:,
+      local: " ",
+      following: [],
+      followers: [],
+      pets: [],
+      posts: [],
+    });
+
+    try {
+      await createdUser.save();
+    } catch (err) {
+      console.log(err);
+      return next();
+    }
+
+    res.json({ message: "Usuario criado com sucesso", user: createdUser });
+  },
+  login: async (req, res, next) => {
+    const { email, password } = req.body;
+
+    // procurando se usuario existe
+    let authenticatedUser;
+    try {
+      authenticatedUser = await User.findOne({ email: email });
+    } catch (err) {
+      return next(err);
+    }
+
+    if (!authenticatedUser) {
+      return res.json({ error: "Usuario nao existe" });
+    }
+
+    // antes de usar o hash na senha
+    if (
+      (bcrypt.compare(req.body.password, authenticatedUser.password),
+      function (err, result) {} !== password)
+    ) {
+      return res.json({ error: "Senha incorreta" });
+    }
+
+    // aqui vai vir a autenticacao
+    let token;
+    try {
+      token = jwt.sign(
+        { userId: authenticatedUser.id, email: authenticatedUser.email },
+        super_secret,
+        { expiresIn: "1hr" }
+      );
+    } catch (err) {
+      console.log(err);
+      return res.json({ error: "Erro interno" });
+    }
+
+    // response
+    res.json({
+      message: "Logged in! - Acessou!",
+      userId: authenticatedUser.id,
+      email: authenticatedUser.email,
+      admin: authenticatedUser.admin,
+      token: token,
+    });
+  },
+  getUser: async (req, res, next) => {
+    const userId = req.params.id;
+
+    let user;
+    try {
+      user = await User.findById(userId, "-password");
+    } catch (error) {}
+
+    if (!user) {
+      return res.json({
+        error: "Nao foi possivel achar o usuario",
+      });
+    }
+
+    res.json({
+      data: user.toObject({ getters: true }),
+    });
+  },
+};
 
 module.exports = UsersControllers;
--- backend/models/user.js
+++ backend/models/user.js
@@ -1,18 +1,30 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const bcrypt = require("bcrypt");
 
 const userSchema = new Schema({
-    admin: { type: Boolean },
-    name: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true, minlength: 6 },
-    image: { type: String, required: true },
-    createdAt: { type: Date, default: Date.now(), required: true },
-    local: { type: String },
-    following: [{ type: mongoose.Types.ObjectId, required: true }],
-    followers: [{ type: mongoose.Types.ObjectId, required: true }],
-    pets: [{ type: mongoose.Types.ObjectId, required: true, ref: 'Pet' }],
-    posts: [{ type: mongoose.Types.ObjectId, required: true, ref: 'Post' }],
+  admin: { type: Boolean },
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true, minlength: 6 },
+  image: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now(), required: true },
+  local: { type: String },
+  following: [{ type: mongoose.Types.ObjectId, required: true }],
+  followers: [{ type: mongoose.Types.ObjectId, required: true }],
+  pets: [{ type: mongoose.Types.ObjectId, required: true, ref: "Pet" }],
+  posts: [{ type: mongoose.Types.ObjectId, required: true, ref: "Post" }],
 });
 
-module.exports = mongoose.model('User', userSchema);
+userSchema.pre("save", async function (next) {
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(this.password, salt);
+    this.password = hashedPassword;
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = mongoose.model("User", userSchema);
